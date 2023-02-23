e238909aae1e52720a2c9f9a6dba7237df3fab59
Ian-Nicky <nickyian04@gmail.com>
2023-02-23 03:14:14 -0800

    display numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers 0 to 9
+ *
+ * Return: numbers 0 to 9
+ */
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 48; d <= 57; d++)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
